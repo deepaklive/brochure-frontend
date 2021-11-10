77f7ebe12cbbd8a4711e71b475c895257c1c578a
Deepak Verma <deepak.verma@live.in>
2021-11-10 17:24:38 +0530

    added one pager routing

--- src/app/brochure/brochure.routing.ts
+++ src/app/brochure/brochure.routing.ts
@@ -3,6 +3,7 @@ import { BrochureMainComponent } from "./brochure-main/brochure-main.component";
 import { BrochureComponent } from "./brochure.component";
 import { EditBrochureComponent } from "./edit-brochure/edit-brochure/edit-brochure.component";
 import { LoginComponent } from "./login/login.component";
+import { OnePagerComponent } from "./one-pager/one-pager.component";
 
 export const DashboardRoutes: Routes = [
   {
@@ -25,4 +26,9 @@ export const DashboardRoutes: Routes = [
     path: "login",
     component: LoginComponent,
   },
+  {
+    path: "one-pager",
+    component: OnePagerComponent,
+  }
+  
 ];
